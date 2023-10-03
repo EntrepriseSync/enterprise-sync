c6a45e038b4b9af04d38a70c4c30b23b0aad964a
dali jerbi <dali.jerbi97@gmail.com>
2023-10-03 07:11:12 +0100

    graphql setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import * as Joi from '@hapi/joi';
 
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DatabaseModule } from './db/db.module';
+import { UsersModule } from './users/users.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
@@ -14,10 +18,21 @@ import { DatabaseModule } from './db/db.module';
         POSTGRES_PASSWORD: Joi.string().required(),
         POSTGRES_DB: Joi.string().required(),
         POSTGRES_CERT: Joi.string().required(),
+        GRAPHQL_PLAYGROUND: Joi.number().required(),
         PORT: Joi.number().required(),
       }),
     }),
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      driver: ApolloDriver,
+      useFactory: (configService: ConfigService) => ({
+        playground: Boolean(configService.get('GRAPHQL_PLAYGROUND')),
+        autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      }),
+    }),
     DatabaseModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
