5a37b9032af47198a51492257f125ce6649fa9d8
dali jerbi <dali.jerbi97@gmail.com>
2023-10-03 05:05:35 +0100

    env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
+import * as Joi from '@hapi/joi';
+
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
